397d1ce9f10930bd443ba9b8adb82d89e6ece625
armaan <adhanji8@gmail.com>
2024-05-01 16:48:44 -0700

    add more questions

---
+++ 22-sorting/index.js
@@ -0,0 +1,10 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+function bubbleSort(arr) {}
+
+function selectionSort(arr) {}
+
+function mergeSort(arr) {}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
